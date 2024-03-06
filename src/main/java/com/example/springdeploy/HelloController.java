54912ce4c8c316137f619ae428cd34af435e434c
Miguel Angel <miguel@MacBook-Air-de-Miguel.local>
2024-03-06 00:06:22 -0600

    Initial commit

---
+++ src/main/java/com/example/springdeploy/HelloController.java
@@ -0,0 +1,40 @@
+package com.example.springdeploy;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @Value("${app.message}")
+    String message;
+
+    @GetMapping(value = "/hola")
+    public String holaMundo(){
+        System.out.println(message);
+        return "Soy Miguel y soy un gran programador y me gusta mucho Spring";
+    }
+
+    @RequestMapping(value = "/bootstrap")
+    public String bootstrap(){
+        return """
+                <!doctype html>
+                <html lang="en">
+                  <head>
+                    <meta charset="utf-8">
+                    <meta name="viewport" content="width=device-width, initial-scale=1">
+                    <title>Bootstrap demo</title>
+                    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+                  </head>
+                  <body>
+                    <h1>Hola Mundo desde Spring</h1>
+                    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+                  </body>
+                </html>
+                """;
+
+    }
+
+}
